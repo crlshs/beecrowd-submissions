f887215b2e1c82fd97ddeb6692fc033e3a55211e
Carlos H. <129799424+drcrls@users.noreply.github.com>
2024-09-24 21:27:06 -0300

    c

--- 3fase bora/apagando.py
+++ 3fase bora/apagando.py
@@ -3,9 +3,16 @@ while True:
     n, d = map(int, input().split())
     if n == 0 and d == 0: break
     numero = list(input())
+    pilha = []
 
-    for _ in range(d):
-        numero.pop(numero.index(min(numero), -1, 0))
-    res.append("".join(numero))
+    for i in numero:
+        while d > 0 and pilha and pilha[-1] < i:
+            pilha.pop()
+            d -= 1
+        pilha.append(i)
 
-print("\n".join(str(i) for i in res))
+    final = pilha[:len(pilha) - d]
+
+    res.append("".join(final))
+
+print("\n".join(res), end="")
